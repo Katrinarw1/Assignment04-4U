a813b31ec3bfd2620e25aa46ffc34ba59c6b9884
watsk8668 <watsk8668@HRH-IC0027308.wrdsb.ca>
2016-11-03 12:04:39 -0400

    comments

--- src/Dalek.java
+++ src/Dalek.java
@@ -20,13 +20,20 @@ public class Dalek {
         this.col = theCol;
 
     }
+    
     // method to move the daleks towards the doctor
-
+    /**
+     * 
+     * @param doc 
+     */
     public void advanceTowards(Doctor doc) {
+        // if this dalek has not crashed into another dalek 
         if (this.crash == false) {
+            // if the doctor is directly below 
             if (this.row < doc.getRow() && this.col == doc.getCol()) {
                 this.row += 1;
-            } else if (this.row > doc.getRow() && this.col == doc.getCol()) {
+            } // if the doctor is directly above
+            else if (this.row > doc.getRow() && this.col == doc.getCol()) {
                 this.row -= 1;
             } else if (this.row < doc.getRow() && this.col < doc.getCol()) {
                 this.row += 1;
@@ -49,18 +56,34 @@ public class Dalek {
     }
 
     // accessor methods
+    /**
+     * Accessor method to get the row that the dalek is on
+     * @return this dalek's row
+     */
     public int getRow() {
         return this.row;
     }
 
+    /**
+     * Accessor method to get the column that the dalek is on
+     * @return this dalek's column
+     */
     public int getCol() {
         return this.col;
     }
 
+    /**
+     * Accessor method to ask whether the dalek has crashed
+     * @return if this dalek has crashed, true if it has crashed, false if it has not crashed
+     */
     public boolean hasCrashed() {
         return this.crash;
     }
 
+    /**
+     * Accessor method to crash the daleks 
+     * @param a the dalek this dalek is intersecting with
+     */
     public void crash(Dalek a) {
         this.crash = true;
     }
