efe5ac5c1dcd8532eb6ad848e69038537143ad28
Peter <Peter@192.168.0.12>
2016-11-10 00:47:07 -0500

    DONE

--- src/Dalek.java
+++ src/Dalek.java
@@ -8,23 +8,29 @@
  * @author watsk8668
  */
 public class Dalek {
-    // instance variables
 
+    // instance variables
     private int row;
     private int col;
     private boolean crash = false;
 
-    // constructor
+    /**
+     * A constructor to create the daleks
+     *
+     * @param theRow an integer that corresponds to a row on the board
+     * @param theCol an integer that corresponds to a column on the board
+     */
     public Dalek(int theRow, int theCol) {
         this.row = theRow;
         this.col = theCol;
 
     }
-    
+
     // method to move the daleks towards the doctor
     /**
-     * 
-     * @param doc 
+     * Moves the dalek towards the doctor
+     *
+     * @param doc
      */
     public void advanceTowards(Doctor doc) {
         // if this dalek has not crashed into another dalek 
@@ -35,21 +41,27 @@ public class Dalek {
             } // if the doctor is directly above
             else if (this.row > doc.getRow() && this.col == doc.getCol()) {
                 this.row -= 1;
-            } else if (this.row < doc.getRow() && this.col < doc.getCol()) {
+            } // if the doctor is below and to the left
+            else if (this.row < doc.getRow() && this.col < doc.getCol()) {
                 this.row += 1;
                 this.col += 1;
-            } else if (this.row > doc.getRow() && this.col < doc.getCol()) {
+            } // if the doctor is above and to the left
+            else if (this.row > doc.getRow() && this.col < doc.getCol()) {
                 this.row -= 1;
                 this.col += 1;
-            } else if (this.row < doc.getRow() && this.col > doc.getCol()) {
+            } // if the doctor is below and to the right
+            else if (this.row < doc.getRow() && this.col > doc.getCol()) {
                 this.row += 1;
                 this.col -= 1;
-            } else if (this.row > doc.getRow() && this.col > doc.getCol()) {
+            } // if the doctor is above and to the right
+            else if (this.row > doc.getRow() && this.col > doc.getCol()) {
                 this.row -= 1;
                 this.col -= 1;
-            } else if (this.row == doc.getRow() && this.col < doc.getCol()) {
+            } // if the doctor is directly to the left 
+            else if (this.row == doc.getRow() && this.col < doc.getCol()) {
                 this.col += 1;
-            } else if (this.row == doc.getRow() && this.col > doc.getCol()) {
+            } // if the doctor is directly to the right
+            else if (this.row == doc.getRow() && this.col > doc.getCol()) {
                 this.col -= 1;
             }
         }
@@ -57,7 +69,8 @@ public class Dalek {
 
     // accessor methods
     /**
-     * Accessor method to get the row that the dalek is on
+     * Returns the row that the dalek is on
+     *
      * @return this dalek's row
      */
     public int getRow() {
@@ -65,7 +78,8 @@ public class Dalek {
     }
 
     /**
-     * Accessor method to get the column that the dalek is on
+     * Returns the column that the dalek is on
+     *
      * @return this dalek's column
      */
     public int getCol() {
@@ -73,18 +87,33 @@ public class Dalek {
     }
 
     /**
-     * Accessor method to ask whether the dalek has crashed
-     * @return if this dalek has crashed, true if it has crashed, false if it has not crashed
+     * Method to determine whether the dalek has crashed
+     *
+     * @param dalek the array of daleks
+     * @return whether the dalek has crashed, true if it has, false if it hasn't
      */
-    public boolean hasCrashed() {
-        return this.crash;
-    }
-
-    /**
-     * Accessor method to crash the daleks 
-     * @param a the dalek this dalek is intersecting with
-     */
-    public void crash(Dalek a) {
-        this.crash = true;
+    public boolean hasCrashed(Dalek dalek[]) {
+        // if this dalek is not dalek 1
+        if (!this.equals(dalek[0])) {
+            // if this dalek is on the same square as dalek 1
+            if (this.getRow() == dalek[0].getRow() && this.getCol() == dalek[0].getCol()) {
+                crash = true;
+            }
+        }
+        // if this dalek is not dalek 2
+        if (!this.equals(dalek[1])) {
+            // if this dalek is on the same square as dalek 2
+            if (this.getRow() == dalek[1].getRow() && this.getCol() == dalek[1].getCol()) {
+                crash = true;
+            }
+        }
+        // if this dalek is not dalek 3
+        if (!this.equals(dalek[2])) {
+            // if this dalek is on the same square as dalek 3
+            if (this.getRow() == dalek[2].getRow() && this.getCol() == dalek[2].getCol()) {
+                crash = true;
+            }
+        }
+        return crash;
     }
 }
--- src/Doctor.java
+++ src/Doctor.java
@@ -8,34 +8,61 @@
  * @author watsk8668
  */
 public class Doctor {
-    // instance variables
 
+    // instance variables
     private int row;
     private int col;
 
-    // constructor
+    /**
+     * A constructor to create a doctor
+     *
+     * @param theRow an integer that corresponds to a row on the board
+     * @param theCol an integer that corresponds to a column on the board
+     */
     public Doctor(int theRow, int theCol) {
         this.row = theRow;
         this.col = theCol;
 
     }
 
-    // to move the doctor
+    /**
+     * Moves the doctor to a specified space around the doctor else to a random
+     * space
+     *
+     * @param newRow an integer that corresponds to a new row on the board
+     * @param newCol an integer that corresponds to a new column on the board
+     */
     public void move(int newRow, int newCol) {
-        if (newRow == row + 1 || newRow == row - 1 || newCol == col + 1 || newCol == col - 1 || (newRow == row + 1 && newCol == col + 1) || (newRow == row + 1 && newCol == col - 1) || (newRow == row - 1 && newCol == col + 1) || (newRow == row - 1 && newCol == col - 1)) {
+        // if the player clicks on the dalek it doesnt move
+        if (newRow == row && newCol == col) {
+            this.row = newRow;
+            this.col = newCol;
+        } // if the player clicks on any of the 8 spaces around the doctor, move the doctor there
+        else if (newRow == row + 1 || newRow == row - 1 || newCol == col + 1 || newCol == col - 1 || (newRow == row + 1 && newCol == col + 1) || (newRow == row + 1 && newCol == col - 1) || (newRow == row - 1 && newCol == col + 1) || (newRow == row - 1 && newCol == col - 1)) {
             row = newRow;
             col = newCol;
-        } else {
+        } // else move the doctor to a random space on the board
+        else {
             this.row = 0 + (int) (Math.random() * (12 - 0));
             this.col = 0 + (int) (Math.random() * (12 - 0));
         }
     }
 
     // accessor methods
+    /**
+     * Returns the row that the doctor is on
+     *
+     * @return the row that the doctor is on
+     */
     public int getRow() {
         return this.row;
     }
 
+    /**
+     * Returns the column that the doctor is on
+     *
+     * @return the column that the doctor is on
+     */
     public int getCol() {
         return this.col;
     }
